e61ffd6e0d82cd11850ea54686f9396890f92fa6
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2024-01-24 15:50:11 +0100

    prova1

--- src/main/java/com/develhope/spring/Users/Users.java
+++ src/main/java/com/develhope/spring/Users/Users.java
@@ -1,4 +1,12 @@
 package com.develhope.spring.Users;
 
+import lombok.Data;
+
+@Data
 public class Users {
+    private String name;
+    private String surname;
+    private String telephoneNumber;
+    private String email;
+    private String password;
 }
